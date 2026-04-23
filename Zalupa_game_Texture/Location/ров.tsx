d5ed5e657010fdb7be6e0d51c0371a488815dcdb
Heiji-Ookubo <dima.smirnow04@gmail.com>
2026-04-23 21:54:21 +0300

    Tail add

---
+++ Zalupa_game_Texture/Location/ров.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="ров" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../Pictures/Pixel Studio/ров.png" width="512" height="512"/>
+ <tile id="54">
+  <objectgroup draworder="index" id="3">
+   <object id="12" x="-0.375" y="0.125" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
